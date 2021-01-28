0d3db855c28b9a7c3448b7c9a94f6d14cd463eed
KonstantIMP <mihedovkos@gmail.com>
2021-01-28 21:44:49 +0300

    Added GCC support

--- source/kernel_build.py
+++ source/kernel_build.py
@@ -1,4 +1,5 @@
 from os import system
+import subprocess
 import platform
 
 def print_hello() :
@@ -51,20 +52,38 @@ def defconfig_choose() :
 def clone_kernel_source() :
     print("Cloning kernel source...")
     system("git clone https://gitlab.com/KonstantIMP/nokia_7_1_stock_kernel.git")
+    system("cd nokia_7_1_stock_kernel")
     print("Done...")
     print("")
 
 def clone_gcc_compiler() :
     print("Cloning GCC compiler (prebuilt by Google):")
-    system("git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9")
+    system("git clone https://github.com/RaghuVarma331/aarch64-linux-android-4.9.git -b master --depth=1 aarch64-linux-android-4.9")
     print("Done...")
     print("")
 
-def get_clang_compiler() :
+def clone_clang_compiler() :
     print("Cloning Clang compiler (prebuilt by Google):")
     system("git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86")
     print("Done...")
     print("")
+
+def gcc_build(d) :
+    print("Build started :")
+    print("  ARCH=arm64")
+    print("  SUBARCH=arm64")
+    print("  CROSS_COMPILE=" + __file__[0:-22] + "aarch64-linux-android-4.9/bin/aarch64-linux-android-")
+    print("  O=output")
+
+    arch = "ARCH=arm64"
+    subarch = "SUBARCH=arm64"
+    cross = "CROSS_COMPILE=" + __file__[0:-22] + "aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+    out = "O=output"
+
+    system("cd nokia_7_1_stock_kernel && sudo make " + out + ' ' + arch + ' ' + subarch + " clean")
+    system("cd nokia_7_1_stock_kernel && sudo make " + out + ' ' + arch + ' ' + subarch + " mrproper")
+    system("cd nokia_7_1_stock_kernel && sudo make " + out + ' ' + arch + ' ' + subarch + (" sdm660_defconfig" if d == 1 else " sdm660-perf_defconfig"))
+    system("cd nokia_7_1_stock_kernel && sudo make " + out + ' ' + arch + ' ' + subarch + ' ' + cross + " -j4")
 
 if __name__ == "__main__" :
     print_hello()
@@ -74,11 +93,19 @@ if __name__ == "__main__" :
     compiler = compiler_choose()
     defconfig = defconfig_choose()
 
+    clone_kernel_source()
+
     if compiler == 1 :
         print("Compiling by GCC")
         print("")
+
+        clone_gcc_compiler()
+
+        gcc_build(defconfig)
     else :
         print("Compiling by Clang")
         print("")
+
+        clone_clang_compiler()
 
     #clone_kernel_source()
