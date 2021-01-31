d20b4f874867cbbc9224f5da2413d8f96b8e808e
KonstantIMP <mihedovkos@gmail.com>
2021-01-31 13:28:10 +0300

    Added images getting

--- source/gsi_port.py
+++ source/gsi_port.py
@@ -15,7 +15,7 @@ def print_hello() :
 
 def os_check() :
     if platform.system() != "Linux" :
-        print("Sorry! You have to use Linux based michine to run the script")
+        print("Sorry! You have to use Linux based machine to run the script")
         print("")
         exit(-1)
 
@@ -33,6 +33,14 @@ def get_rom_name() :
     print('')
     return rom_name
 
+def create_rom_dir(name) :
+    print('Creating working directory...')
+    system('mkdir ' + name + '_working')
+    print('Creating output directory...')
+    system('mkdir ' + name)
+    print('Done!')
+    print('')
+
 def choose_option(choose_name, options_list) :
     print(choose_name + ' :')
     for i in range(len(options_list)) :
@@ -44,14 +52,33 @@ def choose_option(choose_name, options_list) :
         answer = int(input('Enter your answer : '))
 
         if answer <= 0 or answer > len(options_list) :
-            print('  [ERROR] Incorrect number. Try again')
+            print('[ERROR] Incorrect number. Try again')
             print('')
         else :
             print('')
             return answer
 
-def get_vendor() :
+def get_system(s_path, name) :
+    print('Coping system image to work directory...')
+    system('cp ' + s_path + ' ' + name + '_working/system.img')
+
+def get_vendor(name) :
     answer = choose_option('Choose vendor version', ['Stock Android 10 vendor', 'Stock Android 10 vendor modified by RaghuVarma', 'Community Vendor 11'])
+
+    if answer == 1 :
+        system('wget https://gitlab.com/KonstantIMP/gsi_port_resource/-/raw/master/vendor/vendor_stock.img -o ' + name + '_working/vendor.img')
+    elif answer == 2 :
+        system('wget https://gitlab.com/KonstantIMP/gsi_port_resource/-/raw/master/vendor/vendor_stock_by_RaghuVarma.img -o ' + name + '_working/vendor.img')
+    else :
+        system('wget https://gitlab.com/KonstantIMP/gsi_port_resource/-/raw/master/vendor/vendor_community.img -o ' + name + '_working/vendor.img')
+
+    print('')
+
+def get_boot(name) :
+    print('Getting stock boot image...')
+    system('wget https://gitlab.com/KonstantIMP/gsi_port_resource/-/raw/master/boot/boot.img ' + name + '_working/boot.img')
+    print('Done!')
+    print('')
 
 if __name__ == '__main__' :
     print_hello()
@@ -61,4 +88,8 @@ if __name__ == '__main__' :
     gsi_path = get_gsi_path()
     rom_name = get_rom_name()
 
-    get_vendor()
+    create_rom_dir(rom_name)
+
+    get_system(gsi_path, rom_name)
+    get_vendor(rom_name)
+    get_boot(rom_name)
