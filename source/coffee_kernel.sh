eef96884574c4becea0ec6e7a8e18e7093297d65
KonstantIMP <mihedovkos@gmail.com>
2021-02-07 17:09:20 +0300

    Work with coffee kernel (cuppuccino first)

---
+++ source/coffee_kernel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+print_hello() {
+    echo "== == == == == == == == == == == == == == == =="
+    echo "== Woof!!!                                   =="
+    echo "== It is a script for building coffee kernel =="
+    echo "==             Supported devices : Nokia 7.1 =="
+    echo "==                                           =="
+    echo "==    This script was made by KonstantIMP    =="
+    echo "== == == == == == == == == == == == == == == =="
+    echo ""
+}
+
+get_compiler() {
+    
+}
+
+print_hello
